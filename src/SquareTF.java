a02b948a6c759a95e1fe21c51d26f551b6ca7982
Myles Vinh Farr <mylesvf37@gmail.com>
2020-06-25 14:39:25 -0700

    Created class SquareTF

---
+++ src/SquareTF.java
@@ -0,0 +1,70 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+
+public class SquareTF extends JTextField {
+    public final int row, col;
+
+    private int val;
+    private boolean isValid;
+    private boolean isFinal;
+
+    public SquareTF(int row, int col) {
+        super(); //why does it work even if i dont have this
+
+        this.row = row;
+        this.col = col;
+
+        this.val = 0;
+        this.isValid = true;
+        this.isFinal = false;
+
+        setEditable(true);
+        setFormat();
+    }
+
+    public SquareTF(int row, int col, int val) {
+        this.row = row;
+        this.col = col;
+
+        this.val = val;
+        this.isValid = true;
+        this.isFinal = true;
+
+        setEditable(false);
+        setFormat();
+    }
+
+    private void setFormat() {
+        Border squareBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
+        Dimension squareDim = new Dimension(75, 75);
+        Font squareFont = new Font("Monospace", Font.PLAIN, squareDim.height-10);
+
+        setPreferredSize(squareDim);
+        setBorder(squareBorder);
+        setHorizontalAlignment(JTextField.CENTER);
+        setFont(squareFont);
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+        //set valid
+    }
+
+    public void setFinal(boolean isFinal) {
+        //check if val == correct in solved board
+            this.isFinal = isFinal;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public boolean getValid() {
+        return isValid;
+    }
+
+    public boolean getFinal() {
+        return isFinal;
+    }
+}
--- src/SudokuGrid.java
+++ src/SudokuGrid.java
@@ -5,8 +5,8 @@ import java.awt.*;
 public class SudokuGrid extends JPanel{
     private JTextField[][] grid;
 
-    private int size;
-    private int base;
+    private final int size;
+    private final int base;
 
     private JPanel[][] subGridPanels;
     //private JPanel boardPanel;
@@ -16,16 +16,29 @@ public class SudokuGrid extends JPanel{
         this.size = size;
         this.base = (int) Math.sqrt(size);
 
-        initGrid();
-
         this.subGridPanels = new JPanel[base][base];
 
         setLayout(new GridLayout(base, base));
 
+        initTFGrid();
+        initPanGrid();
+    }
+
+    private void initTFGrid() {
+        for(int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                JTextField square = new SquareTF(i, j);
+
+                grid[i][j] = square;
+            }
+        }
+    }
+
+    private void initPanGrid() {
         for(int i = 0; i < base; i++) {
             for(int j = 0; j < base; j++) {
                 JPanel subGrid = new JPanel(new GridLayout(base, base));
-                subGrid.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
+                subGrid.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
 
                 subGridPanels[i][j] = subGrid;
                 add(subGrid);
@@ -35,27 +48,6 @@ public class SudokuGrid extends JPanel{
         for(int i = 0; i < size; i++) {
             for(int j = 0; j < size; j++) {
                 subGridPanels[i/base][j/base].add(grid[i][j]);
-            }
-        }
-
-
-    }
-
-    private void initGrid() {
-        Border squareBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
-        Dimension squareDim = new Dimension(30, 30);
-        Font squareFont = new Font("Verdana", Font.PLAIN, 20);
-
-        for(int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                JTextField square = new JTextField();
-
-                square.setPreferredSize(squareDim);
-                square.setBorder(squareBorder);
-                square.setHorizontalAlignment(JTextField.CENTER);
-                square.setFont(squareFont);
-
-                grid[i][j] = square;
             }
         }
     }
